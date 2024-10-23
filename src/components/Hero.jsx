ae9ecaeae4d7962a865d712873739370278df5a7
idoz1 <169044973+idoz1@users.noreply.github.com>
2024-10-23 20:36:55 +0300

    added some functionality

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero-container'>
+      <div className='hero-center'>
+        <h1>
+          Manage Any Content <br /> Anywhere
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+          perspiciatis cum molestiae quaerat, quia eaque dolorum delectus ab ea
+          aut in nisi consectetur id dolor quae incidunt debitis. Quam, eos.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Submenu.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import sublinks from '../data'
+import { useGlobalContext } from '../Context'
+
+const Submenu = () => {
+  const { pageId } = useGlobalContext()
+  const currentPage = sublinks.find((item) => item.pageId === pageId)
+
+  return (
+    <div className='submenu'>
+      <h5>{currentPage?.page}</h5>
+      <div
+        className='submenu-links'
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? '1fr 1fr' : '1fr',
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link
+          return (
+            <a href={url} key={id}>
+              {icon}
+              {label}
+            </a>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Submenu
